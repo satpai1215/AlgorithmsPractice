620221687b49b3a502ee07cf7c2bc3dc54771131
satpai1215 <pai.satyan@gmail.com>
2014-04-01 20:46:55 -0700

    EulerPyramidProblem

--- Deques and Queues/src/Deque.java
+++ Deques and Queues/src/Deque.java
@@ -1,4 +1,46 @@
+import java.util.Iterator;
 
-public class Deque {
 
+public class Deque<Item> implements Iterable<Item> {
+   public Deque() {
+	   // construct an empty deque
+   }
+   public boolean isEmpty() {
+	   // is the deque empty?
+   }
+   public int size() {
+	   // return the number of items on the deque
+   }
+   public void addFirst(Item item) {
+	   // insert the item at the front
+   }
+   public void addLast(Item item) {
+	   // insert the item at the end
+   }
+   public Item removeFirst() {
+	   // delete and return the item at the front
+   }
+   public Item removeLast() {
+	   // delete and return the item at the end
+   }
+   public Iterator<Item> iterator() {
+	   // return an iterator over items in order from front to end
+   }
+   
+   private class DequeIterator implements Iterator<Item>
+   {
+	   public boolean hasNext() {
+	   }
+	   
+	   public Item next() {
+		   
+	   }
+	   
+	   public void remove() {
+		   throw new java.util.Uns;
+	   }
+   }
+   public static void main(String[] args) {
+	   // unit testing
+   }
 }
---
+++ EulerPyramidProblem/src/MaxSumPyramid.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class MaxSumPyramid {
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader("tri.txt"));
+		
+		//pyramid is an arrayList where each row is represented by a String[]
+		ArrayList<String[]> pyramid = new ArrayList<String[]>();
+		
+		String rowString = reader.readLine();		
+		
+		//0. read file, populate pyramid
+		while (rowString != null) {
+			pyramid.add(rowString.split(" "));
+			rowString = reader.readLine();
+		}
+		reader.close();
+		
+		//1. Start at second to last row of pyramid (rowTop)
+		//2. For each num in rowTop, check left and right child in rowBottom, add max to self
+		//3. Delete last row
+		//4. Continue upward until you reach first row.
+		
+		int size = pyramid.size() - 1;
+		for(int i = size; i > 0; i-- ) {
+			String[] rowTop = pyramid.get(i-1);
+			String[] rowBottom = pyramid.get(i);
+			
+			for(int x = 0; x < rowTop.length ; x++) {
+				int parent = Integer.parseInt(rowTop[x]);
+				int leftChild = Integer.parseInt(rowBottom[x]); 
+				int rightChild = Integer.parseInt(rowBottom[x+1]);
+				
+				rowTop[x] = String.valueOf(parent + Math.max(leftChild, rightChild));
+			}
+			pyramid.remove(i);
+		}
+		System.out.println(pyramid.get(0)[0]);
+	}
+
+}
